8a4ade9150343283e40ba0ce8b59ba2b0d413254
Jorge Guerrero <giorg0s@pm.me>
2020-04-27 20:48:23 +0200

    Añadido el contenido de la practica anterior

---
+++ deteccion_haar.py
@@ -0,0 +1,57 @@
+# /usr/bin/python3
+
+import cv2
+import time
+import os
+import numpy as np
+from deteccion_orb import carga_imagenes_carpeta
+
+CARPETA_TEST = 'img/test'
+CLASIFICADOR = 'assets/haar/coches.xml'
+
+# Se crea el cascade classifier
+cascade = cv2.CascadeClassifier(CLASIFICADOR)
+
+
+def procesamiento_img_haar(imagen, contador):
+    # Se convierte la imagen a escala de grises
+    gray = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+
+    # El código se basa en los ejemplos que se encuentran en la web: https://www.programcreek.com/python/example/79435/cv2.CascadeClassifier
+    imagen_eq = cv2.equalizeHist(gray)
+
+    imagen_gris = cascade.detectMultiScale(imagen_eq, scaleFactor=1.05, minNeighbors=5, minSize=(50, 50))
+
+    if imagen_gris is ():
+        print('Error')
+    for (x, y, w, h) in imagen_gris:
+        imagen_rect = cv2.rectangle(imagen, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.imshow('Detector de coches', imagen_rect[y:y+h, x:x+w])
+
+        cv2.waitKey(1)
+        # time.sleep(1)
+
+    # cv2.imwrite(os.path.join('img', 'output', 'output_haar'+str(contador)+'.png'), imagen)
+
+    # cv2.destroyAllWindows()
+
+
+def detector_coches(imagenes):
+    tiempos = []
+    for i, img in enumerate(imagenes):
+        inicio = time.time()
+        print("PROCESANDO IMAGEN", i)
+        procesamiento_img_haar(img, i)
+        fin = time.time()
+        tiempos.append(fin-inicio)
+
+    print('TIEMPO MEDIO POR IMAGEN', sum(tiempos)/len(imagenes))
+
+
+def main():
+    test_imgs = np.array(carga_imagenes_carpeta(CARPETA_TEST)) # se cargan las imagenes de test
+    detector_coches(test_imgs)
+
+
+if __name__ == '__main__':
+    main()
--- leer_coche.py
+++ leer_coche.py
@@ -3,3 +3,34 @@
 import cv2
 import sklearn
 import numpy as np
+from deteccion_orb import *
+from deteccion_haar import *
+
+CLASIFICADOR_MATRICULAS = 'assets/haar/matriculas.xml'
+
+cascade_matriculas = cv2.CascadeClassifier(CLASIFICADOR_MATRICULAS)
+
+def detecta_matriculas(imagenes):
+    # frontales = [] # array con los frontales detectados
+    for i, img in enumerate(imagenes):
+        frontal_coche = procesamiento_img_haar(img, i)
+        img_procesada = cascade_matriculas.detectMultiScale(frontal_coche, scaleFactor=1.05, minNeighbors=5, minSize=(50, 50))
+
+        if img_procesada is ():
+            print('Error')
+        for (x, y, w, h) in img_procesada:
+            imagen_rect = cv2.rectangle(frontal_coche, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            cv2.imshow('Detector de coches', imagen_rect[y:y + h, x:x + w])
+
+            cv2.waitKey(1)
+            # time.sleep(1)
+
+
+def main():
+    # Carga de imagenes
+    test_imgs = carga_imagenes_carpeta(CARPETA_TEST)
+    detecta_matriculas(test_imgs)
+
+
+if __name__ == '__main__':
+    main()
